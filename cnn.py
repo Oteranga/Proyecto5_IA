fcd536cf7c28f10cbbab63835127cceca29ef8b3
angelicasanchezbarreto <angelica.sanchez@utec.edu.pe>
2021-12-04 19:52:38 -0500

    fixing training

--- cnn.py
+++ cnn.py
@@ -24,12 +24,12 @@ class CNN(nn.Module):
             nn.Conv2d(8, 16, kernel_size=5, stride=1, padding=2),
             nn.BatchNorm2d(16),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=4))
+            nn.MaxPool2d(kernel_size=2, stride=2))
         self.layer3 = nn.Sequential(
             nn.Conv2d(16, 32, kernel_size=7, stride=1, padding=2),
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=5))
+            nn.MaxPool2d(kernel_size=2, stride=2))
         
     def just_dropout(self):
         self.layer1 = nn.Sequential(
@@ -41,12 +41,12 @@ class CNN(nn.Module):
             nn.Conv2d(8, 16, kernel_size=5, stride=1, padding=2),
             nn.Dropout(0.2),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=4))
+            nn.MaxPool2d(kernel_size=2, stride=2))
         self.layer3 = nn.Sequential(
             nn.Conv2d(16, 32, kernel_size=7, stride=1, padding=2),
             nn.Dropout(0.2),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=5))
+            nn.MaxPool2d(kernel_size=2, stride=2))
     
     def batch_and_drop(self):
         self.layer1 = nn.Sequential(
@@ -58,11 +58,11 @@ class CNN(nn.Module):
         self.layer2 = nn.Sequential(
             nn.Conv2d(8, 16, kernel_size=5, stride=1, padding=2),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=4))
+            nn.MaxPool2d(kernel_size=2, stride=2))
         self.layer3 = nn.Sequential(
             nn.Conv2d(16, 32, kernel_size=7, stride=1, padding=2),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=5))            
+            nn.MaxPool2d(kernel_size=2, stride=2))            
 
     def forward(self, x):
         out = self.layer1(x)
@@ -77,72 +77,88 @@ class CNN(nn.Module):
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-def training(model,optimizer,loss_func,num_epochs,training_loader, testing_loader, early_stop):
+def training(model,optimizer,loss_func,num_epochs,training_loader, validation_loader, early_stop):
     print('Training CNN...')
-    # for early stopping
-    the_last_loss = 100
-    patience = 2
-    trigger_times = 0
-    # for loss
+    # for training
     total_step = len(training_loader)
-    loss_list = []
+    loss_training = []
+    # for validation
+    loss_validation = []
+    # for early stopping
+    last_val_loss = 100
     for epoch in range(num_epochs):
+        current_train_loss = 0
         for i, (images, labels) in enumerate(training_loader):
+            # for training
             images = images.to(device)
             labels = labels.to(device)
             out = model(images)
             loss = loss_func(out,labels)
-            
             optimizer.zero_grad() #zero the gradient
             loss.backward()
             optimizer.step() #updates
             
-            loss_list.append(loss.item())
+            current_train_loss += loss.item()
             
             if(i+1)%100 == 0:
                 print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
                     .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
         
-        if early_stop is True:
-            loss_list,the_current_loss = testing(model, loss_func, testing_loader)
-            print('The current loss:', the_current_loss)
-            if the_current_loss > the_last_loss:
-                trigger_times += 1
-                print('trigger times:', trigger_times)
-                if trigger_times >= patience:
-                    print('Early stopping!\nStart to test process.')
-                    return model
-            else:
-                print('trigger times: 0')
-                trigger_times = 0
-            the_last_loss = the_current_loss
+        loss_training.append(current_train_loss/len(training_loader))
+        
+        current_val_loss = validation(model,loss_func,validation_loader)
+        loss_validation.append(current_val_loss)
 
-    print('Finished Training Trainset')
-    return loss_list
+        if early_stop is True:
+            last_val_loss = early_stopping(model,current_val_loss,last_val_loss)
+
+    print('Finished Training CNN.')
+    return loss_training, loss_validation
+
+def early_stopping(model, current_val_loss, last_val_loss):
+    trigger_times = 0
+    patience = 5
+    print('The current loss:', current_val_loss)
+    if current_val_loss > last_val_loss:
+        trigger_times += 1
+        print('trigger times:', trigger_times)
+        if trigger_times >= patience:
+            print('Early stopping!\nStart to test process.')
+            return model
+    else:
+        print('trigger times: 0')
+        trigger_times = 0
+    return current_val_loss
+
+# CNN VALIDATION FUNC
+
+def validation(model,loss_func,validation_loader):
+    #model.eval()
+    current_val_loss = 0
+    with torch.no_grad():
+        for images, labels in validation_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            loss = loss_func(outputs, labels)
+            current_val_loss += loss.item()
+
+    return current_val_loss / len(validation_loader)
 
 # CNN TESTING FUNC #
 
-def testing(model, loss_func, testing_loader):
+def testing(model, testing_loader):
     print('Testing CNN...')
-    model.eval()
-    # for loss
-    loss_list = []
-    loss_total = 0
-    # for accuracy
     correct = 0
     total = 0
     with torch.no_grad():
         for images, labels in testing_loader:
             images = images.to(device)
             labels = labels.to(device)
-            out = model(images)
-            # for loss
-            loss = loss_func(out,labels)            
-            loss_list.append(loss.item())
-            loss_total += loss.item()
-            # for accuracy
-            _, predicted = torch.max(out.data, 1)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
         print('Accuracy of the model on the test images: {} %'.format(100 * correct / total))
-        return loss_list, loss_total/len(testing_loader)
+    print('Finished Testing CNN.')
--- main.py
+++ main.py
@@ -11,50 +11,24 @@ training_set, validation_set, testing_set = rd.get_data_sets(
 training_loader, validation_loader, testing_loader = rd.get_data_loaders(
     training_set, validation_set, testing_set)
 
-# CNN TRAINING #
+# EXPERIMENTS #
 
 num_classes = 4
 learning_rate = 0.001
 num_epochs = 15
 
-""" model = cnn.CNN(num_classes, "batch").to(cnn.device)
-loss_func = cnn.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
-loss_list = cnn.train(model, optimizer, loss_func, num_epochs,training_loader)
-pl.plot_error(loss_list,"error_batch","(Using Batch Normalization)") """
-
-# TESTS #
-
-def test_batch_norm():
-    model = cnn.CNN(num_classes, "batch").to(cnn.device)
+def experiment(exp_info):
+    model = cnn.CNN(num_classes, exp_info[0]).to(cnn.device)
     loss_func = cnn.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
-    train_loss = cnn.training(model, optimizer, loss_func, num_epochs, training_loader, testing_loader, False)
-    test_loss, accuracy = cnn.testing(model,loss_func,testing_loader)
-    pl.plot_error(train_loss,test_loss,"t1_error_batch","(Using Batch Normalization)")
+    training_loss, validation_loss = cnn.training(
+        model, optimizer, loss_func, num_epochs, training_loader, validation_loader, False)
+    cnn.testing(model,loss_func,testing_loader)
+    pl.plot_error(training_loss,validation_loss,exp_info[1],exp_info[2])
 
-def test_dropout():
-    model = cnn.CNN(num_classes, "dropout").to(cnn.device)
-    loss_func = cnn.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
-    train_loss = cnn.training(model, optimizer, loss_func, num_epochs, training_loader, testing_loader, False)
-    test_loss, accuracy = cnn.testing(model,loss_func,testing_loader)
-    pl.plot_error(train_loss,test_loss,"t1_error_dropout","(Using Dropout)")
+exp_batch_info = ["batch","t1_error_batch","(Using Batch Normalization)"]
+exp_dropout_info = ["dropout","t1_error_dropout","(Using Dropout)"]
+exp_batch_drop_info = ["both","t1_error_batch_drop","(Using Dropout and Batch Normalization)"]
+exp_early_stop_info = ["dropout","t1_error_early_stop","(Using Early Stopping)"]
 
-def test_batch_drop():
-    model = cnn.CNN(num_classes, "both").to(cnn.device)
-    loss_func = cnn.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
-    train_loss = cnn.training(model, optimizer, loss_func, num_epochs, training_loader,testing_loader,False)
-    test_loss, accuracy = cnn.testing(model,loss_func,testing_loader)
-    pl.plot_error(train_loss,test_loss,"t1_error_batch_drop","(Using Dropout and Batch Normalization)")
-
-def test_early_stop():
-    model = cnn.CNN(num_classes, "dropout").to(cnn.device)
-    loss_func = cnn.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
-    train_loss = cnn.training(model, optimizer, loss_func, num_epochs, training_loader,testing_loader,True)
-    test_loss, accuracy = cnn.testing(model,loss_func,testing_loader)
-    pl.plot_error(train_loss,test_loss,"t1_error_early_stop","(Using Early Stopping)")
-
-test_batch_norm()
+experiment(exp_batch_info)
